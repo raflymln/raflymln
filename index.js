caca5cfcd4209a566b1896c8030d9266a025efc8
Rafly Maulana <mraflymaulana@gmail.com>
2021-03-30 21:43:49 +0700

    clean && updates

--- index.js
+++ index.js
@@ -1,70 +1,91 @@
+const fs = require('fs');
 const http = require('http');
 const express = require('express');
-const fs = require('fs');
-const { exec } = require("child_process");
 const bodyParser = require('body-parser');
-const crypto = require('crypto');
 
 const app = express();
 const server = http.createServer(app);
 
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(async(req, res, next) => {
-    const requestIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    console.log(`[${new Date().toLocaleString()}] ${requestIp} (${req.method}) | New Request on: ${req.path}`);
-    next();
-});
-
-app.get('/', (req, res) => {
-    res.render('pages/home');
-});
-
-// Tes dulu
-app.post('/git-update', (req, res) => {
-    console.log(`[${new Date().toLocaleString()}] (Github) Receiving Repository Update`);
+const validateSignature = (request, key) => {
+    const crypto = require('crypto');
 
     const expectedSignature = "sha1=" +
-        crypto.createHmac("sha1", 'yakalibisatausecretiniwkwkwk')
+        crypto.createHmac("sha1", key)
         .update(JSON.stringify(req.body))
         .digest("hex");
 
     const signature = req.headers["x-hub-signature"];
     if (signature !== expectedSignature) {
-        throw new Error("Invalid signature.");
+        return false;
     }
 
-    console.log(`[${new Date().toLocaleString()}] (Github) Webhook Validated`);
-    res.sendStatus(200)
+    return true;
+}
 
-    const bash = "cd /var/www/raflymaulana && git stash && git pull && npm i && pm2 restart all";
+const requestLogger = (req, res, next) => {
+    const requestIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    log(`${requestIp} (${req.method})`, `New Request on: ${req.path}`);
+    next();
+}
+
+const log = (logger, logtext) => {
+    return console.log(`[${new Date().toLocaleString()}]`, `${logger} |`, logtext);
+}
+
+const execute = (bash) => {
+    const { exec } = require("child_process");
+
+    console.log('============ BEGIN CHILD PROCCESS ============');
     exec(bash, (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`);
             return;
         }
+
         if (stderr) {
             console.log(`stderr: ${stderr}`);
             return;
         }
+
         console.log(`stdout: ${stdout}`);
+        console.log('============ END CHILD PROCCESS ============')
     });
+
+    return true;
+}
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(requestLogger);
+
+app.get('/', (req, res) => {
+    res.render('pages/home');
+});
+
+app.post('/git-update', (req, res) => {
+    log("Github", "Receiving Repository Update Hooks");
+
+    if (validateSignature(req, "yakalibisatausecretiniwkwkwk")) {
+        log("Github", "Webhook Signature Validated");
+        execute("cd /var/www/raflymaulana && git stash && git pull && npm i && pm2 restart all");
+        return res.sendStatus(200);
+    } else {
+        log("Github", "Webhook Signature Invalid");
+        return res.sendStatus(403);
+    }
 });
 
 app.post('/projects', (req, res) => {
-    var data;
-
     try {
         const json = fs.readFileSync(`projects.json`, "utf8");
-        data = JSON.parse(json);
+        const data = JSON.parse(json);
+        return res.send(data);
     } catch (error) {
-        console.log(error)
-        throw new Error('Error JSON')
+        log("SYSTEM", error);
+        throw new Error('Failed to Read JSON');
     }
-
-    res.send(data);
 })
 
-server.listen(3000, () => console.log(`Website is Connected!`));
+server.listen(3000, () => log("SYSTEM", "Process is Now Online on Port 3000!"));
