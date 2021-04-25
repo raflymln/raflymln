145170dbc8f985e7117197ab681279dc5a40520f
Rafly Maulana <mraflymaulana@gmail.com>
2021-04-25 23:05:07 +0700

    edit socials

--- index.js
+++ index.js
@@ -89,5 +89,34 @@ app.post('/projects', (req, res) => {
     }
 });
 
+app.get('/social/:social', (req, res) => {
+    const social = (param) => {
+        switch (param) {
+            case 'github':
+                return 'https://github.com/raflymln';
+            case 'instagram':
+                return 'https://www.instagram.com/raflymln_/';
+            case 'twitter':
+                return 'https://twitter.com/raflymln';
+            case 'linkedin':
+                return 'https://www.linkedin.com/in/raflymln/';
+            case 'dribbble':
+                return 'https://dribbble.com/raflymln';
+            case 'soundcloud':
+                return 'https://soundcloud.com/rafly_maulana';
+            case 'spotify':
+                return 'https://open.spotify.com/artist/7AT29mk5bZImdbEgdp7w2q';
+            case 'youtube':
+                return 'https://www.youtube.com/channel/UCswhauY4r6p3RXgKsVnOlnQ';
+            case 'discord':
+                return 'https://discord.com/users/231721153444446208';
+            default:
+                return '/';
+        }
+    }
+
+    return res.redirect(social(req.params.social));
+})
+
 
 server.listen(3000, () => log("SYSTEM", "Process is Now Online on Port 3000!"));
