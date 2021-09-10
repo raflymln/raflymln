d410d0f58f26315dadbbe8177ce0282bf9a9e395
Rafly Maulana <mraflymaulana@gmail.com>
2021-09-10 21:09:50 +0700

    Add meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -28,9 +28,26 @@ export default function App({ Component, pageProps }: AppProps) {
                 <title>Rafly Maulana</title>
                 <link rel="icon" href="/favicon.ico" />
                 <link rel="manifest" crossOrigin="use-credentials" href="/manifest.json" />
+
+                {/* Primary Meta Tags */}
                 <meta name="viewport" content="initial-scale=1" />
+                <meta name="title" content="Rafly Maulana" />
                 <meta name="description" content="Rafly Maulana was a fullstack developer, designer, and producer, from Indonesia." />
                 <meta name="theme-color" content="#57A773" />
+
+                {/* Open Graph / Facebook */}
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://raflymaulana.me/" />
+                <meta property="og:title" content="Rafly Maulana" />
+                <meta property="og:description" content="Rafly Maulana was a fullstack developer, designer, and producer, from Indonesia." />
+                <meta property="og:image" content="https://raflymaulana.me/images/banner.png" />
+
+                {/* Twitter */}
+                <meta property="twitter:card" content="https://raflymaulana.me/images/banner.png" />
+                <meta property="twitter:url" content="https://raflymaulana.me/" />
+                <meta property="twitter:title" content="Rafly Maulana" />
+                <meta property="twitter:description" content="Rafly Maulana was a fullstack developer, designer, and producer, from Indonesia." />
+                <meta property="twitter:image" content="https://raflymaulana.me/images/banner.png" />
             </Head>
 
             <ScrollToTop>
