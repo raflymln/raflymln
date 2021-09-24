abf0f41de6970f5c419132116c8b66ea5eaacad5
Rafly Maulana <mraflymaulana@gmail.com>
2021-09-24 15:16:56 +0700

    Add simple animation and state for scroll to top button

--- src/components/ScrollToTopButton.tsx
+++ src/components/ScrollToTopButton.tsx
@@ -6,6 +6,7 @@ import * as Images from "@/components/Images";
 const Button = () => {
     const [isRocketActive, setRocketActiveState] = useState(false);
     const [isOpen, setOpenState] = useState(false);
+    const [isHittingBottom, setIfHittingBottom] = useState(false);
 
     const scrollToTop = () => {
         scrollTo(0, 1000);
@@ -53,6 +54,12 @@ const Button = () => {
             } else {
                 setOpenState(false);
             }
+
+            if (window.innerHeight + Math.ceil(window.pageYOffset) >= document.body.offsetHeight) {
+                setIfHittingBottom(true);
+            } else {
+                setIfHittingBottom(false);
+            }
         });
     });
 
@@ -63,8 +70,10 @@ const Button = () => {
                 <Link href="#0" passHref={true}>
                     <button
                         onClick={scrollToTop}
-                        className="btn fixed right-8 bottom-8 z-50 animate-shake duration-100 bg-theme-blue-very-dark text-white !rounded-full animate-iteration-[1] animate-duration-[0.5s]">
-                        <span className="bg-theme-blue-dark !p-5 !rounded-full">
+                        className={`btn fixed right-8 bottom-8 z-50 animate-shake duration-100 text-white !rounded-full animate-duration-[0.5s] ${
+                            isHittingBottom ? "animate-iteration-infinite bg-red-600" : "bg-theme-blue-very-dark animate-iteration-[1]"
+                        }`}>
+                        <span className={`!p-5 !rounded-full ${isHittingBottom ? "bg-red-500" : "bg-theme-blue-dark"}`}>
                             <svg className="text-white fill-current w-5 h-auto" width="43" height="43" viewBox="0 0 43 43" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M23.65 38.7H19.35V12.9L10.75 21.5L6.44995 19.35L21.5 4.29999L36.5499 19.35L32.2499 21.5L23.65 12.9V38.7Z" />
                             </svg>
